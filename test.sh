b625919a3997b21d7bc99138c763b6e6070dcbb1
user <user@client1.localdomain>
2020-04-20 22:14:22 -0400

    basics

---
+++ test.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+
+echo Hello World
+
+echo "Hello World"
+
+echo 'Hello World'
+
+===================
+export PATH=$PATH:/home/user/naeem
+===================================
+Three ways to run a program
+1. bash programname
+2. ./programname
+3. make the part of path environment variable
+and then we can run just by giving program file name
+
+Example:
+    test.sh
+Output: Hello World
