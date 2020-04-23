81d7f68ec32ade83209f866e0ad1f77405d38147
zeeshanali6091@gmail.com <user@client1.localdomain>
2020-04-23 14:06:10 -0400

    string_basic

---
+++ prog1.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+#string,digit,float
+
+#debugging,comment multiple line
+<<naeem
+strijgljlg
+this is my comment line 1
+this is my comment line 2
+this is my comment line 3
+echo "Hello"
+echo 'Hello'
+echo Hello
+
+echo "Hello"
+echo 'Hello'
+echo Hello
+echo
+
+Hello=10
+echo "$Hello"
+echo '$Hello'
+echo $Hello
+naeem
+#==============
+<<string1
+echo "My laptop price is $500"
+echo 'My laptop price is $500'
+
+student=naeem
+echo "first student name is: $student"
+echo 'first student name is: $student'
+string1
+#==========================
+
+echo "i need "double qoutes" in this line"
+echo
+echo "After adding new solution"
+echo
+echo "i need 'double qoutes' in this line"
+
+echo
+
+
+
+# escaping special charachters
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ testy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+while true;
+do
+echo "Muneeb I'm a gost"
+sleep 2
+echo "I WILL EAT YOU"
+sleep 2
+done
