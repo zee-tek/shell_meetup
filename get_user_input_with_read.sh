928df6473f7ab99a50e0e65fbe8cd76cda352ebf
zeeshanali6091@gmail.com <user@client1.localdomain>
2020-05-13 14:15:08 -0400

    Study

---
+++ get_user_input_with_read.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#==========================
+# Program To Get User input
+# Author Naeem
+#****************************
+# Print a message to user to enter his/her name
+# Store that name into a variable
+# Print variable
+
+
+#echo -e "Please Enter Your Name\n"
+
+#read user_info
+
+
+#echo -e "\nYou Enter Name $user_info"
+
+
+
+echo -e "Please Enter Your Password\n"
+
+read  -s user_info
+
+echo "You Password is: $user_info"
